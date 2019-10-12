6554b97efdc61bd4e2e9685fa85ed007c8c34e91
Unknown <tom.buehler1997@gmail.com>
2019-10-12 15:33:41 +0200

    Camera follows Player

---
+++ Running out of Space (Cake)/Assets/Scripts/PlayerEconomy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEconomy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float startingMoney = 420.69f;
+    public float currentMoney;
+    public int currentCakes;
+    public bool broke;
+    public float cakePrize = 10f;
+
+    private Animator anim;
+    private PlayerMovement playerMovement;
+    
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        currentMoney = startingMoney;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReceiveMoney(float sum)
+    {
+        currentMoney += sum;
+    }
+
+    public void LoseMoney(float sum)
+    {
+        currentMoney -= sum;
+
+        if (currentMoney < 0f)
+        {
+            broke = true;
+        }
+    }
+
+    public void BuyCake()
+    {
+        int count = (int)(currentMoney / cakePrize);
+        float rest = currentMoney % cakePrize;
+
+        currentCakes += count;
+        currentMoney = rest;
+    }
+}
